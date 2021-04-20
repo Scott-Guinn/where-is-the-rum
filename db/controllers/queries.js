9c61beed9493346bdc41b53b7585717ce26e81b4
Scott Guinn <guinnscott111@gmail.com>
2021-04-20 12:17:45 -0600

    altered reference to API Key (no longer destructuring)

--- db/controllers/queries.js
+++ db/controllers/queries.js
@@ -1,6 +1,6 @@
 const db = require('../index.js');
 const axios = require('axios');
-const {API_KEY} = process.env.API_KEY;
+const API_KEY = process.env.API_KEY;
 const User = require('../models/user.js');
 const { calculateBearing } = require('../../helpers/calculateBearing.js');
 
